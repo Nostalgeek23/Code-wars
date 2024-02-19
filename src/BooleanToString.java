dfce608d5d50911b293ff922620f6d0771e9aeef
Eugene <dorick23@gmail.com>
2024-02-19 18:21:07 +0300

    Convert a Boolean to a String

---
+++ src/BooleanToString.java
@@ -0,0 +1,10 @@
+public class BooleanToString {
+//
+//    Implement a function which convert the given boolean value into its string representation.
+//
+//    Note: Only valid inputs will be given.
+
+    public static String convert(boolean b) {
+        return String.valueOf(b);
+    }
+}
