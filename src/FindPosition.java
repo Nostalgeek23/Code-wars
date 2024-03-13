70fe62de75609a5c28e2511aed142588195fe457
Eugene <dorick23@gmail.com>
2024-03-13 20:54:15 +0300

    The Feast of Many Beasts

---
+++ src/FindPosition.java
@@ -0,0 +1,11 @@
+public class FindPosition {
+//    When provided with a letter, return its position in the alphabet.
+//
+//            Input :: "a"
+//
+//    Ouput :: "Position of alphabet: 1"
+    public static String position(char alphabet)
+    {
+        return "Position of alphabet: " + ((int) alphabet - 96);
+    }
+}
