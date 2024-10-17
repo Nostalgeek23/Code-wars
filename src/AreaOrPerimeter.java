8bd095f20d50a0486eaee89f1a643965030b27a1
Eugene <dorick23@gmail.com>
2024-10-17 21:55:55 +0300

    Switch it up

---
+++ src/AreaOrPerimeter.java
@@ -0,0 +1,8 @@
+public class AreaOrPerimeter {
+//  You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+//  If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+  public static int areaOrPerimeter (int l, int w) {
+    return l == w ? l * w : 2 * l + 2 * w;
+  }
+}
